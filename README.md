264c61f8fc402731354c707369f494d8f34c252a
swonny <hanbul0921@hanmail.net>
2021-11-26 16:01:28 +0900

    "21.11.26(금) sleep, eat, play 버튼 구현"

--- README.md
+++ README.md
@@ -7,3 +7,23 @@
 - 다마고치 클래스 생성
   - class Tama
   - 메소드 생성 eat(), sleep(), play()
+
+- 알게된 것
+  - AVD 새로고침 하는법: 실행정지 -> 재실행
+  - 뷰(View) : 앱 화면을 구성하는 요소들을 뷰(View)라고 칭한다.
+  - layout : 뷰의 배치방식을 정의한 것
+  - Button 위치 변경하는 방법
+  - LinearLayout : 뷰를 한쪽 방향으로 차곡차곡 쌓는 것을 의미한다.
+  - 방향을 지정하는 속성은 android:orientation
+  - 선택 가능한 방향은 위에서 아래(↓): vertical, 또는 왼쪽에서 오른쪽 방향(→):horizontal
+    ex. <LinearLayout android:orientation="vertical"> ... 버튼 추가 ... </LinearLayout>
+        => html의 태그와 비슷한 개념!
+  - MainActivity 클래스 내부의 onCreate는 '앱이 생성되는 시기에'라는 의미를 가짐
+
+  - 안드로이드 xml과 class 연결하기
+    1) MainActivity 파일 상단에 위젯(widget)값 선언
+       ex. Button large,small;//Widget Field값 선언 (Button)
+           TextView textview; //Widget Field값 선언 (TextView)
+    2) onCreate에 findViewById를 통해 연결
+
+
--- app/src/main/java/com/example/javaproject/MainActivity.java
+++ app/src/main/java/com/example/javaproject/MainActivity.java
@@ -6,19 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+Button eatBtn, playBtn, sleepBtn;
+TextView tamaName;
+
 class Tama {
     private String name;
     private int level;
     private int energy;
 
-    public Tama(String name){
+    public Tama(String name){ // init
         this.name = name;
         this.level = 1;
-        this.energy = 10;
+        this.energy = 15;
     }
 
     public void eat() {
-
+        energy += 2;
     }
 
     public void play() {
@@ -36,9 +39,72 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eatBtn =
     }
 
-    public void on1(View v) {
-        Toast.makeText(getApplicationContext(), "버튼 클릭 테스트", Toast.LENGTH_SHORT).show();
+    public void eat(View v) {
+        Toast.makeText(getApplicationContext(), "점심시간!", Toast.LENGTH_SHORT).show();
     }
+
+    public void play(View v) {
+        Toast.makeText(getApplicationContext(), "뭐하고 놀까?", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sleep(View v) {
+        Toast.makeText(getApplicationContext(), "z Z z z Z ..", Toast.LENGTH_SHORT).show();
+    }
+
+//            // 레이아웃 생성
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout .setOrientation(LinearLayout.VERTICAL);
+//
+//        // 레이아웃 파라미터 생성
+//        LinearLayout.LayoutParams linearLayoutParams =
+//                new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                );
+//
+//        // 텍스트뷰를 생성한 후 파라미터 설정
+//        TextView textView = new TextView(this);
+//        textView.setText("Text View");
+//        textView.setLayoutParams(linearLayoutParams);
+//
+//        // 생성한 TextView 를 linearLayout 에 추가
+//        linearLayout .addView(textView);
+//
+//        // 화면에 표시되도록 setContentView 실
+//        setContentView(linearLayout);
+
+//    @Override
+//
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        // 레이아웃 생성
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout .setOrientation(LinearLayout.VERTICAL);
+//
+//        // 레이아웃 파라미터 생성
+//        LinearLayout.LayoutParams linearLayoutParams =
+//                new LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                );
+//
+//        // 텍스트뷰를 생성한 후 파라미터 설정
+//        TextView textView = new TextView(this);
+//        textView.setText("Text View");
+//        textView.setLayoutParams(linearLayoutParams);
+//
+//        // 생성한 TextView 를 linearLayout 에 추가
+//        linearLayout .addView(textView);
+//
+//        // 화면에 표시되도록 setContentView 실
+//        setContentView(linearLayout);
+//    }
+
+
 }
+

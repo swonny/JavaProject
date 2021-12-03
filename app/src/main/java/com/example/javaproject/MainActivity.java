14d1b923a03813950ba1494592d74b474bb7aea2
swonny <hanbul0921@hanmail.net>
2021-12-03 16:44:26 +0900

    21.12.03 (금) 이름 입력받기 버튼생성, 캐릭터 객체 생성 구현

--- app/src/main/java/com/example/javaproject/MainActivity.java
+++ app/src/main/java/com/example/javaproject/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -43,36 +45,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button startButton = (Button) findViewById(R.id.startBtn); // 화면전환 이벤트
-        startButton.setOnClickListener(new View.OnClickListener() {
+        TextView characterName = (TextView) findViewById(R.id.characterName);
+        EditText scanName = (EditText) findViewById(R.id.scanName);
+        startButton.setOnClickListener(new View.OnClickListener() { // 시작 버튼 화면전환
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), SubActivity.class);
                 startActivity(intent);
-            }
+                 characterName.setText(scanName.getText());
 
+//                String tName = new String(scanName.getText());
+//                Tama myChar = new Tama();
+            }
+        });
+
+        Button sendNameButton = (Button) findViewById(R.id.sendName);
+        sendNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tName = new String(String.valueOf(scanName.getText()));
+                Toast.makeText(getApplicationContext(), tName+" 생성!", Toast.LENGTH_SHORT).show();
+                Tama myCharacter = new Tama(tName);
+
+                System.out.println(myCharacter.name);
+            }
         });
 
     }
     Scanner scan = new Scanner(System.in);
-
-    public void eat(View v) {
-        Toast eatToast = Toast.makeText(getApplicationContext(), "점심시간!", Toast.LENGTH_SHORT);
-        eatToast.show();
-        System.out.println("tesss");
-    }
-
-    public void play(View v) {
-        Toast.makeText(getApplicationContext(), "뭐하고 놀까?", Toast.LENGTH_SHORT).show();
-    }
-
-    public void sleep(View v) {
-        Toast.makeText(getApplicationContext(), "z Z z z Z ..", Toast.LENGTH_SHORT).show();
-    }
-
-    public void gameStart(View v) {
-        Tama my = new Tama("hi");
-        System.out.printf("%s", my.name);
-    }
 
 //            // 레이아웃 생성
 //        LinearLayout linearLayout = new LinearLayout(this);
--- app/src/main/java/com/example/javaproject/SubActivity.java
+++ app/src/main/java/com/example/javaproject/SubActivity.java
@@ -1,6 +1,8 @@
 package com.example.javaproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,24 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_main);
+    }
+
+    public void eat(View v) {
+        Toast eatToast = Toast.makeText(getApplicationContext(), "점심시간!", Toast.LENGTH_SHORT);
+        eatToast.show();
+        System.out.println("tesss");
+    }
+
+    public void play(View v) {
+        Toast.makeText(getApplicationContext(), "뭐하고 놀까?", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sleep(View v) {
+        Toast.makeText(getApplicationContext(), "z Z z z Z ..", Toast.LENGTH_SHORT).show();
+    }
+
+    public void gameStart(View v) {
+        Tama my = new Tama("hi");
+        System.out.printf("%s", my.name);
     }
 }

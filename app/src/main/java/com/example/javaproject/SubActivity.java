3a47068d119f426b8fb57d4db22903ff6c71e472
swonny <hanbul0921@hanmail.net>
2021-12-06 01:20:17 +0900

    21.12.06 월 add funcs, add energy&level managing func, add imageView, add animation

--- app/src/main/java/com/example/javaproject/SubActivity.java
+++ app/src/main/java/com/example/javaproject/SubActivity.java
@@ -1,10 +1,14 @@
 package com.example.javaproject;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,35 +35,73 @@ public class SubActivity extends AppCompatActivity {
         characterName = (TextView) findViewById(R.id.characterName);
         myCharacter = (Tama) intent.getSerializableExtra("myTama"); // 객체 불러오기
 
-        characterName.setText("이름 : "+ myCharacter.name + "\n레벨 : " + myCharacter.getLevel() + "\n에너지 : "+ myCharacter.getEnergy());
+        characterName.setText("이름      : "+ myCharacter.name + "\n레벨      : " + myCharacter.getLevel() + "\n에너지  : "+ myCharacter.getEnergy());
 
-        //카운트다운 타이머 시작
-        TextView sleepTimerView = (TextView) findViewById(R.id.sleepTimer);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView); // 이미지뷰
+        imageView.setImageResource(R.drawable.usual);
+
+        TextView sleepTimerView = (TextView) findViewById(R.id.sleepTimer); // 자는 시간 타이머 뷰
         Button sleepButton = (Button) findViewById(R.id.sleepButton);
         sleepButton.setOnClickListener(new View.OnClickListener() { // 잠자기 버튼 클릭시 타이머 시작
             @Override
             public void onClick(View view) {
-                CountDownTimer countDownTimer = new CountDownTimer(30000,1000) {
+                CountDownTimer countDownTimer = new CountDownTimer(20000,1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
                         int num = (int) (millisUntilFinished / 1000);
-                        sleepTimerView.setText(Integer.toString(num+1) + "초 뒤에 일어날게요!😴");
+                        sleepTimerView.setText(Integer.toString(num + 1) + "초 뒤에 일어날게요!😴");
+                        if((num+1) <= 1) {
+                            sleepTimerView.setText("잠에서 깨어났어요!");
+                            imageView.setImageResource(R.drawable.usual);
+                        } else if((num+1)%3 == 0) {
+                            imageView.setImageResource(R.drawable.sleep);
+                        } else if ((num+1)%3 == 2) {
+                            imageView.setImageResource(R.drawable.sleep2);
+                        } else if ((num+1)%3 == 1) {
+                            imageView.setImageResource(R.drawable.sleep3);
+                        }
                     }
 
                     @Override
                     public void onFinish() {
                         myCharacter.sleep();
                         setInfoTextview();
-                        sleepTimerView.setText("상태 : 잠에서 깨어났어요!");
+                        sleepTimerView.setText("");
+                        imageView.setImageResource(R.drawable.usual);
                     }
                 }.start();
             }
         });
 
+        Button eatButton = (Button) findViewById(R.id.eatButton);
+        eatButton.setOnClickListener(new View.OnClickListener() { // 잠자기 버튼 클릭시 타이머 시작
+            @Override
+            public void onClick(View view) {
+                CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        int num = (int) (millisUntilFinished / 1000);
+                        if((num+1)%2 == 0) {
+                            imageView.setImageResource(R.drawable.eat);
+                        } else if ((num+1)%2 == 1) {
+                            imageView.setImageResource(R.drawable.eat2);
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        myCharacter.sleep();
+                        setInfoTextview();
+                        sleepTimerView.setText("");
+                        imageView.setImageResource(R.drawable.usual);
+                    }
+                }.start();
+            }
+        });
     }
 
     public void setInfoTextview() {
-        characterName.setText("이름 : "+ myCharacter.name + "\n레벨 : " + myCharacter.getLevel() + "\n에너지 : "+ myCharacter.getEnergy());
+        characterName.setText("이름      : "+ myCharacter.name + "\n레벨      : " + myCharacter.getLevel() + "\n에너지  : "+ myCharacter.getEnergy());
     }
 
     public void eat(View v) {
@@ -73,6 +115,11 @@ public class SubActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "뭐하고 놀까?", Toast.LENGTH_SHORT).show();
         myCharacter.play();
         setInfoTextview();
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView); // 이미지뷰
+        imageView.setImageResource(R.drawable.background);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.move);
+        imageView.startAnimation(animation);
     }
 
 }
